98b1b901d29d6755d51ace20570463c0ff267e49
Hawken Rives <hawkrives@gmail.com>
2011-11-10 12:41:14 -0600

    Integrated wordcount into showdown-gui

--- showdown-gui.js
+++ showdown-gui.js
@@ -183,9 +183,30 @@ function convertText() {
 //	Event handlers
 //
 
+function count(this_field) {
+	var char_count = this_field.value.length;
+	var fullStr = this_field.value + " ";
+	var initial_whitespace_rExp = /^[^A-Za-z0-9]+/gi;
+	var left_trimmedStr = fullStr.replace(initial_whitespace_rExp, "");
+	var non_alphanumerics_rExp = rExp = /[^A-Za-z0-9']+/gi;
+	var cleanedStr = left_trimmedStr.replace(non_alphanumerics_rExp, " ");
+	var splitString = cleanedStr.split(" ");
+	var word_count = splitString.length -1;
+	
+	if (fullStr.length <2) { word_count = 0; }
+
+	if (word_count == 1) { wordOrWords = " word"; }
+	else { wordOrWords = " words"; }
+
+	if (char_count == 1) { charOrChars = " character"; } 
+	else { charOrChars = " characters"; }
+
+
+	document.getElementById('counted').innerHTML = word_count + wordOrWords + "," + "    " + char_count + charOrChars + ",";
+}
+
 function onConvertTextSettingChanged() {
-	// If the user just enabled automatic
-	// updates, we'll do one now.
+	// If the user just enabled automatic updates, we'll do one now.
 	onInput();
 }
 
